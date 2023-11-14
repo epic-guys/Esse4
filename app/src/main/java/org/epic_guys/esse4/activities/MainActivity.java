681f97995cecb0a2ffad871731b1d9de401ed122
wapeety <wapeety@protonmail.com>
2023-11-14 17:54:15 +0100

    Adding account, broken :)

--- app/src/main/java/org/epic_guys/esse4/activities/MainActivity.java
+++ app/src/main/java/org/epic_guys/esse4/activities/MainActivity.java
@@ -4,7 +4,11 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.epic_guys.esse4.API.API;
 
 import org.epic_guys.esse4.R;
 
@@ -20,14 +24,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AccountManager am = AccountManager.get(this);
-        Account[] accounts = am.getAccountsByType("org.epic_guys.esse4");
-        if (accounts.length == 0) {
-            launchLoginActivity();
-        } else {
-            // Manage account, get token, etc.
-            // if login fails, delete account and return to login activity
-        }
+            AccountManager am = AccountManager.get(this);
+            Account[] accounts = am.getAccountsByType("org.epic_guys.esse4");
+            Log.i("MainActivity", "Accounts: " + accounts.length);
+            if (accounts.length == 0) {
+                Log.i("MainActivity", "No accounts found");
+                launchLoginActivity();
+            } else {
+                Log.i("MainActivity", "Account found");
+                // Manage account, get token, etc.
+                // if login fails, delete account and return to login activity
+            }
 
         setContentView(R.layout.activity_main);
 

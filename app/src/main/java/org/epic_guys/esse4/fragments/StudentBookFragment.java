2cfa3ae269cc03bd97d1e9c14f28226d7be81192
Simon Scoltock <simon.scoltock.work@gmail.com>
2023-12-04 18:41:01 +0100

    creazione fragment libretto

---
+++ app/src/main/java/org/epic_guys/esse4/fragments/StudentBookFragment.java
@@ -0,0 +1,47 @@
+package org.epic_guys.esse4.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.fragment.NavHostFragment;
+
+import org.epic_guys.esse4.R;
+
+public class StudentBookFragment extends Fragment {
+    private NavController navController;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        navController = NavHostFragment.findNavController(this);
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_student_book, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //when back button is pressed, go back to home fragment
+        view.findViewById(R.id.btn_back).setOnClickListener(v -> {
+            NavOptions navOptions = new NavOptions.Builder()
+                    .setPopUpTo(R.id.homeFragment, true)
+                    .build();
+            navController.navigate(R.id.homeFragment, null, navOptions);
+        });
+    }
+}

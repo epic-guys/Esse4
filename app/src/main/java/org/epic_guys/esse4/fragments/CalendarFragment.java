8cb28d3304b38608140785a5c1cac1b24141187b
wapeety <wapeety@protonmail.com>
2024-01-06 00:49:45 +0100

    implemented day-by-day swipe gesture

--- app/src/main/java/org/epic_guys/esse4/fragments/CalendarFragment.java
+++ app/src/main/java/org/epic_guys/esse4/fragments/CalendarFragment.java
@@ -1,7 +1,11 @@
 package org.epic_guys.esse4.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
@@ -35,6 +39,9 @@ public class CalendarFragment extends Fragment {
     private NavController navController;
     private CalendarView calendarView;
     private Calendar calendar;
+    final int SWIPE_THRESHOLD = 100;
+    final int SWIPE_VELOCITY_THRESHOLD = 100;
+    private final HashMap<String, ArrayList<AppelloLibretto>> appelliPerData = new HashMap<>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,6 +55,7 @@ public class CalendarFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -61,7 +69,6 @@ public class CalendarFragment extends Fragment {
 
         ArrayList<AppelloLibretto> appelliTotali = new ArrayList<>();
 
-        ListView listView = view.findViewById(R.id.list_view);
         TextView selectedDate = view.findViewById(R.id.selected_date);
         selectedDate.setText(Common.setCalendar(calendar, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)));
 
@@ -75,7 +82,6 @@ public class CalendarFragment extends Fragment {
                     initCalendar(calendarView, appelliList);
 
                     appelliTotali.addAll(appelliList);
-                    HashMap<String, ArrayList<AppelloLibretto>> appelliPerData = new HashMap<>();
 
                     for (AppelloLibretto appello : appelliTotali) {
                         if(appello.getDataOraEsame() == null) {
@@ -86,13 +92,7 @@ public class CalendarFragment extends Fragment {
                     }
                     Common.stopLoading(requireView().findViewById(R.id.page), requireView(), R.id.loading);
 
-                    calendarView.setOnDateChangeListener((view1, year, month, dayOfMonth) -> {
-                        selectedDate.setText(Common.setCalendar(calendar, year, month, dayOfMonth));
-
-                        String dataSelezionata = LocalDate.of(year, month + 1, dayOfMonth).format(formatter);
-                        ArrayList<AppelloLibretto> appelliGiorno = appelliPerData.getOrDefault(dataSelezionata, new ArrayList<>());
-                        listView.setAdapter(new ListAdapter(requireContext(), appelliGiorno));
-                    });
+                    calendarView.setOnDateChangeListener((view1, year, month, dayOfMonth) -> goToDay(LocalDate.of(year, month + 1, dayOfMonth)));
                 });
 
 
@@ -103,6 +103,78 @@ public class CalendarFragment extends Fragment {
                     .build();
             navController.navigate(R.id.homeFragment, null, navOptions);
         });
+
+        //when btn_next is pressed, go to next day
+        view.findViewById(R.id.btn_next_day).setOnClickListener(v -> goToNextDay());
+
+        //when btn_previous is pressed, go to previous day
+        view.findViewById(R.id.btn_prev_day).setOnClickListener(v -> goToPreviousDay());
+
+        View dayLayout = requireView().findViewById(R.id.day_layout);
+
+        GestureDetector gestureDetector = new GestureDetector(requireContext(), new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1,@NonNull MotionEvent e2, float velocityX, float velocityY) {
+                Log.i("TAG", "onFling: " + e1.toString() + e2);
+                float diffX = e2.getX() - e1.getX();
+                float diffY = e2.getY() - e1.getY();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        dayLayout.setOnTouchListener(
+                (v, event) -> gestureDetector.onTouchEvent(event)
+        );
+    }
+
+
+
+    private void onSwipeRight() {
+        goToPreviousDay();
+    }
+
+    private void onSwipeLeft() {
+        goToNextDay();
+    }
+
+    private void goToNextDay() {
+        CalendarView calendarView = requireView().findViewById(R.id.calendar);
+        LocalDate date = LocalDate.ofEpochDay(calendarView.getDate() / (24 * 60 * 60 * 1000));
+        goToDay(date.plusDays(1));
+    }
+
+    private void goToPreviousDay() {
+        CalendarView calendarView = requireView().findViewById(R.id.calendar);
+        LocalDate date = LocalDate.ofEpochDay(calendarView.getDate() / (24 * 60 * 60 * 1000));
+        goToDay(date.minusDays(1));
+    }
+
+    private void goToDay(@NonNull LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        CalendarView calendarView = requireView().findViewById(R.id.calendar);
+        ListView listView = requireView().findViewById(R.id.list_view);
+        TextView selectedDate = requireView().findViewById(R.id.selected_date);
+
+        calendarView.setDate(date.toEpochDay() * 24 * 60 * 60 * 1000);
+        int year = date.getYear();
+        int month = date.getMonthValue() - 1;
+        int dayOfMonth = date.getDayOfMonth();
+
+        selectedDate.setText(Common.setCalendar(calendar, year, month, dayOfMonth));
+
+        String dataSelezionata = LocalDate.of(year, month + 1, dayOfMonth).format(formatter);
+        ArrayList<AppelloLibretto> appelliGiorno = appelliPerData.getOrDefault(dataSelezionata, new ArrayList<>());
+        listView.setAdapter(new ListAdapter(requireContext(), (appelliGiorno == null) ? new ArrayList<>() : appelliGiorno));
     }
 
     private void initCalendar(CalendarView calendarView, List<AppelloLibretto> appelliList) {
